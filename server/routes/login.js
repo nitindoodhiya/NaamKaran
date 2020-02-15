e9ee2940f2fb6982e25181e29e1131f2c22fb8c2
Siddhi-Mahawar <34399383+Siddhi-Mahawar@users.noreply.github.com>
2020-02-15 19:32:50 +0530

    Update login.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var mysql      = require('mysql');
+var spawn = require('child_process').spawn;
+var py    = spawn('python', ['input1.py']);
+var moment = require('moment');
+var fs = require('fs');
+var request = require('request');
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 var md5 = require('md5');
 var nodemailer = require('nodemailer');
 const session = require('express-session');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'socstop'
+});
 
-
-var sess; // global session, NOT recommended
-// var session = require('express-session');
+var con = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : '',
+   database : 'socstop'
+ });
+ 
 var transporter = nodemailer.createTransport({
    service: 'gmail',
    auth: {
@@ -17,45 +34,3 @@ var transporter = nodemailer.createTransport({
      pass: 'euphony@123'
    }
  });
- 
-var con = mysql.createConnection({
-   host: "localhost",
-   user: "root",
-   password: "",
-   database:"socstop"
- });
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'socstop'
-});
-router.get('/login',(req,res,err)=>{
-   res.render('index');
-});
-router.get('/register',(req,res,err)=>{
-   res.render('signup');
-});
-router.post('/register', function(req, res, next) {
-   var det = req.body;
-   var email  = det.username;
-   var password = det.password;
-   var q = "INSERT INTO users VALUES('"+email+"','"+password+"')";
-   connection.query(q,(err,result)=>{
-      if (err) {
-         throw err;
-
-      }
-      else{
-         req.session.email = email;
-         
-         console.log('registered');
-         res.render('dashboard');
-         
-      }
-   });
-   
-});
-
-module.exports = router;
